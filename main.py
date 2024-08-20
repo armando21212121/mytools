25aa88b7f0d8d0e07b79f5cc44af56389027aee4
Armando <souzaarmando16@gmail.com>
2024-08-20 15:42:25 -0300

    Adiciona módulo MYTOOLS com constantes e funções

---
+++ main.py
@@ -0,0 +1,9 @@
+import MYTOOLS
+
+
+# Usando as funções
+print("pi_real(5):", MYTOOLS.pi_real(5))
+print("e_real(4):", MYTOOLS.e_real(4))
+
+print("PI_INT:", MYTOOLS.PI_INT)
+print("E_INT:", MYTOOLS.E_INT)
